d5137a34489d6ec7466e50d04ecc22da04d9fd09
krishramoju2 <futureai.incorporated@gmail.com>
2026-04-02 19:56:29 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from pymongo import MongoCliente
+from pymongo import MongoClient
 from bson import ObjectId
 import datetime
 import jwt
