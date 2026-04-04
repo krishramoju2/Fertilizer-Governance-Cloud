399d84a4efdb12e0886d62720208b638fa21881e
krishramoju2 <futureai.incorporated@gmail.com>
2026-04-04 22:01:12 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,89 +4,13 @@ import Dashboard from "./pages/Dashboard/Dashboard";
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
   const [token, setToken] = useState(null);
-  const [message, setMessage] = useState({ text: "", type: "" });
 
-  const [inputs, setInputs] = useState({
-    Temperature: "",
-    Moisture: "",
-    Soil_Type: "",
-    Crop_Type: "",
-    Fertilizer_Name: ""
-  });
-
-  const [soilTypes, setSoilTypes] = useState([]);
-  const [cropTypes, setCropTypes] = useState([]);
-  const [fertilizerNames, setFertilizerNames] = useState([]);
-
-  const [loading, setLoading] = useState(false);
-  const [result, setResult] = useState(null);
-
-  const [history, setHistory] = useState([]);
-  const [analytics, setAnalytics] = useState({});
-
-
- 
-
-  const handleAnalyze = () => {
-    console.log("Analyze clicked");
-  };
-
-
-  
-
-  
-  
-  console.log({
-    token,
-    message,
-    soilTypes,
-    cropTypes,
-    fertilizerNames,
-    loading,
-    result,
-    history,
-    analytics
-  });
-  
   return (
-
-  <Dashboard
-    currentUser={currentUser}
-    setToken={setToken}
-    setCurrentUser={setCurrentUser}
-  
-    message={message}
-    setMessage={setMessage}
-  
-    inputs={inputs}
-    setInputs={setInputs}
-  
-    soilTypes={soilTypes}
-    setSoilTypes={setSoilTypes}
-  
-    cropTypes={cropTypes}
-    setCropTypes={setCropTypes}
-  
-    fertilizerNames={fertilizerNames}
-    setFertilizerNames={setFertilizerNames}
-  
-    loading={loading}
-    setLoading={setLoading}
-  
-    result={result}
-    setResult={setResult}
-  
-    history={history}
-    setHistory={setHistory}
-  
-    analytics={analytics}
-    setAnalytics={setAnalytics}
-
-    handleAnalyze={handleAnalyze}
-      
-  />
-
-    
+    <Dashboard
+      currentUser={currentUser}
+      setCurrentUser={setCurrentUser}
+      setToken={setToken}
+    />
   );
 }
 
