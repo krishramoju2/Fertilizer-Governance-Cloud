42c6c61d30eaee170455e7cae4a81951cbee41fe
krishramoju2 <futureai.incorporated@gmail.com>
2026-04-09 10:01:45 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// ✅ ADD THIS IMPORT
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    {/* ✅ WRAP APP HERE */}
+    <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
