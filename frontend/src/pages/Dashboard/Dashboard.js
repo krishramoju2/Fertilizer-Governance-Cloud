7ed008dd48431d87b7828e012f199614fd1af69b
krishramoju2 <futureai.incorporated@gmail.com>
2026-04-09 21:51:04 +0530

    Update Dashboard.js

--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -5,7 +5,7 @@ import Chatbot from "../../components/Chatbot/Chatbot";
 import MLModel from "../../components/ML/MLModel";
 import api from "../../services/api";
 import Home from "../../components/Home/Home";
-import InfiniteMenu from "../../components/InfiniteMenu/InfiniteMenu";
+import InfiniteMenu from "../../components/Home/InfiniteMenu";
 
 const styles = {
   
