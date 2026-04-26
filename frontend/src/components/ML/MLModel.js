9ee5d610f69681c7c8bc5153ab23eb5e906785d5
krishramoju2 <futureai.incorporated@gmail.com>
2026-04-27 03:06:42 +0530

    Update MLModel.js

--- frontend/src/components/ML/MLModel.js
+++ frontend/src/components/ML/MLModel.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import api from "../../services/api";
 import GradientBlinds from "./GradientBlinds";
-import { motion } from "framer-motion";
+
 
 export default function MLModel() {
   const [form, setForm] = useState({
